8cbd04f4ae73aa2b9ee17b353bac2b4ebbb28e03
KononenkoAsya <ananaskononenko@gmail.com>
2018-06-28 13:08:23 +0300

    installation and configuring ESLint

--- src/index.js
+++ src/index.js
@@ -1,49 +1,52 @@
-import { getRandomCocktail } from './api'
-import _ from 'lodash'
+/* global document */
+import _ from 'lodash';
+import { getRandomCocktail } from './api';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('root')
+  const root = document.getElementById('root');
 
   _.times(8).forEach(async () => {
-    const { body: { drinks } } = await getRandomCocktail()
+    const { body: { drinks } } = await getRandomCocktail();
 
-    drinks.forEach(({ strDrink,strInstructions, strDrinkThumb, ...ingredients}) => {
+    drinks.forEach(({
+      strDrink, strInstructions, strDrinkThumb, ...ingredients
+    }) => {
+      const drinkBlock = document.createElement('div');
+      drinkBlock.setAttribute('class', 'drinks-block root__drinks-block');
 
-      const drinkBlock = document.createElement('div')
-      drinkBlock.setAttribute('class','drinks-block root__drinks-block')
 
-      const drinksNames = document.createElement('div')
-      drinksNames.setAttribute('class','drinks-names drinks-block__drinks-names')
-      const drinkText = document.createTextNode(strDrink)
-      drinksNames.appendChild(drinkText)
+      const drinksNames = document.createElement('div');
+      drinksNames.setAttribute('class', 'drinks-names drinks-block__drinks-names');
+      const drinkText = document.createTextNode(strDrink);
+      drinksNames.appendChild(drinkText);
 
-      const drinksImages = document.createElement('img')
-      drinksImages.setAttribute('class','drink-image drinks-block__drink-image')
-      drinksImages.setAttribute('src',strDrinkThumb)
+      const drinksImages = document.createElement('img');
+      drinksImages.setAttribute('class', 'drink-image drinks-block__drink-image');
+      drinksImages.setAttribute('src', strDrinkThumb);
 
-      const ingredientsList = document.createElement('ul')
-      ingredientsList.setAttribute('class','drink-ingredient drinks-block__drink-ingredient')
+      const ingredientsList = document.createElement('ul');
+      ingredientsList.setAttribute('class', 'drink-ingredient drinks-block__drink-ingredient');
 
-      const instructionBlock = document.createElement('div')
-      const drinkInstruction = document.createTextNode(strInstructions)
-      instructionBlock.appendChild(drinkInstruction)
-      instructionBlock.setAttribute('class','instruction-block drinks-block__instruction-block')
+      const instructionBlock = document.createElement('div');
+      const drinkInstruction = document.createTextNode(strInstructions);
+      instructionBlock.appendChild(drinkInstruction);
+      instructionBlock.setAttribute('class', 'instruction-block drinks-block__instruction-block');
 
-      root.appendChild(drinkBlock)
-      drinkBlock.appendChild(drinksNames)
-      drinkBlock.appendChild(drinksImages)
-      drinkBlock.appendChild(instructionBlock)
+      root.appendChild(drinkBlock);
+      drinkBlock.appendChild(drinksNames);
+      drinkBlock.appendChild(drinksImages);
+      drinkBlock.appendChild(instructionBlock);
 
-      let filteredArray = Object.keys(ingredients).filter((ingredienttsFilter) => ingredienttsFilter.match(/^strIngredient\w*$/))
+      const filteredArray = Object.keys(ingredients).filter(ingredienttsFilter => ingredienttsFilter.match(/^strIngredient\w*$/));
       filteredArray.forEach((keyIngredient) => {
-        if(ingredients[keyIngredient]) {
-          let li = document.createElement('li')
-          let listItem = document.createTextNode(ingredients[keyIngredient])
-          li.appendChild(listItem)
-          ingredientsList.appendChild(li)
+        if (ingredients[keyIngredient]) {
+          const li = document.createElement('li');
+          const listItem = document.createTextNode(ingredients[keyIngredient]);
+          li.appendChild(listItem);
+          ingredientsList.appendChild(li);
         }
-      })
-      drinkBlock.appendChild(ingredientsList)
-      })
-    })
-  })
+      });
+      drinkBlock.appendChild(ingredientsList);
+    });
+  });
+});
