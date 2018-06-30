307c2ff212c44f92ec378d2e64df961dc70fd6de
KononenkoAsya <ananaskononenko@gmail.com>
2018-06-30 20:20:06 +0300

    change directory of css file and images. using the right BEM technology

--- src/index.js
+++ src/index.js
@@ -5,43 +5,49 @@ import _ from 'lodash';
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
 
+  const titleIngredients = document.createElement('p');
+  const titleText = document.createTextNode('Cocktails card ')
+  titleIngredients.setAttribute('class', 'title-ingredients');
+  titleIngredients.appendChild(titleText);
+  root.appendChild(titleIngredients);
+  
   _.times(8).forEach(async () => {
     const { body: { drinks } } = await getRandomCocktail()
 
     drinks.forEach(({ strDrink, strInstructions, strDrinkThumb, ...ingredients}) => {
 
       const drinkBlock = document.createElement('div');
-      drinkBlock.setAttribute('class', 'drinks-block root__drinks-block');
+      drinkBlock.setAttribute('class', 'drinks-block');
+
+      const imageBlock = document.createElement('div');
+      const drinksImages = document.createElement('img');
+      imageBlock.setAttribute('class', 'image-block drinks-block__image-block');
+      drinksImages.setAttribute('class', 'drink-image image-block__drink-image');
+      drinksImages.setAttribute('src', strDrinkThumb);
+      imageBlock.appendChild(drinksImages);
+
+      const drinkInfo = document.createElement('div');
+      drinkInfo.setAttribute('class', 'info-block drinks-block__info-block');
 
       const drinksNames = document.createElement('div');
       drinksNames.setAttribute('class', 'drinks-names info-block__drinks-names');
       const drinkText = document.createTextNode(strDrink);
       drinksNames.appendChild(drinkText);
 
-      const imageBlock = document.createElement('div');
-      const drinksImages = document.createElement('img');
-      imageBlock.setAttribute('class', 'block-image drinks-block__drink-image');
-      drinksImages.setAttribute('class', 'drink-image block-image__drink-image');
-      drinksImages.setAttribute('src', strDrinkThumb);
-      imageBlock.appendChild(drinksImages);
-
       const ingredientsList = document.createElement('ul');
       ingredientsList.setAttribute('class', 'drink-ingredient list-block__drink-ingredient');
       const listBlock = document.createElement('div');
       listBlock.setAttribute('class', 'list-block info-block__list-block');
 
+      const textIngredients = document.createElement('p')
+      const text = document.createTextNode('Ingredients: ')
+      textIngredients.setAttribute('class', 'text-ingredient info-block__text-ingredient');
+      textIngredients.appendChild(text);
+
       const instructionBlock = document.createElement('div');
       const drinkInstruction = document.createTextNode(strInstructions);
       instructionBlock.appendChild(drinkInstruction);
       instructionBlock.setAttribute('class', 'instruction-block info-block__instruction-block');
-
-      const drinkInfo = document.createElement('div');
-      drinkInfo.setAttribute('class', 'info-block drinks-block__info-block');
-
-      const textIngredients = document.createElement('p')
-      const text = document.createTextNode('Ingredients: ')
-      textIngredients.setAttribute('class', 'text-ingredient info-block__info-block');
-      textIngredients.appendChild(text);
 
       root.appendChild(drinkBlock);
       drinkBlock.appendChild(imageBlock);
@@ -52,6 +58,7 @@ document.addEventListener('DOMContentLoaded', () => {
       filteredArray.forEach((keyIngredient) => {
         if(ingredients[keyIngredient]) {
           let li = document.createElement('li');
+          li.setAttribute('class', 'list-ingredient drink-ingredient__list-ingredient');
           let listItem = document.createTextNode(ingredients[keyIngredient]);
           li.appendChild(listItem);
           ingredientsList.appendChild(li);
@@ -61,7 +68,6 @@ document.addEventListener('DOMContentLoaded', () => {
       listBlock.appendChild(ingredientsList);
       drinkInfo.appendChild(listBlock);
       drinkInfo.appendChild(instructionBlock);
-      console.log(drinksNames);
       });
     });
   });
